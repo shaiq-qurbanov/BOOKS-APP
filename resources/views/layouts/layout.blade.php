2f15b21f1f056f220106a08a14530c2041c6ea2a
shaiq-qurbanov <shaiqqurbanov73@gmail.com>
2022-05-31 17:17:03 +0400

    reneme client.js

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,7 +8,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <title>Document</title>
     <script src="/js/app.js"></script>
-    <script src="/js/client.js"></script>
+    <script src="/js/student.js"></script>
 </head>
 <body>
  @section('header') 
